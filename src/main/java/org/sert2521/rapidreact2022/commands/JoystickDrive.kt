672d395650b79bf8d770ff60e42bc492322d0682
kturner44 <kturner44@4j.lane.edu>
2022-01-21 16:31:44 -0800

    Basic drivetrain code

---
+++ src/main/java/org/sert2521/rapidreact2022/commands/JoystickDrive.kt
@@ -0,0 +1,7 @@
+package org.sert2521.rapidreact2022.commands
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase
+
+object JoystickDrive : SubsystemBase() {
+
+}
---
+++ src/main/java/org/sert2521/rapidreact2022/subsytems/Drivetrain.kt
@@ -0,0 +1,67 @@
+package org.sert2521.rapidreact2022.subsytems
+
+import com.ctre.phoenix.motorcontrol.ControlMode
+import com.ctre.phoenix.motorcontrol.FeedbackDevice
+import com.ctre.phoenix.motorcontrol.can.TalonSRX
+import edu.wpi.first.wpilibj2.command.SubsystemBase
+import org.sert2521.rapidreact2022.Motors
+
+object Drivetrain : SubsystemBase() {
+    private val frontLeft = TalonSRX(Motors.FRONT_LEFT.id)
+    private val backLeft = TalonSRX(Motors.BACK_LEFT.id)
+
+    private val frontRight = TalonSRX(Motors.FRONT_RIGHT.id)
+    private val backRight = TalonSRX(Motors.BACK_RIGHT.id)
+
+    init {
+        frontRight.inverted = true
+        backRight.inverted = true
+
+        frontLeft.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative)
+        frontRight.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative)
+    }
+
+    private fun spinLeft(amount: Double) {
+        frontLeft.set(ControlMode.PercentOutput, amount)
+        backLeft.set(ControlMode.PercentOutput, amount)
+    }
+
+    private fun spinRight(amount: Double) {
+        frontRight.set(ControlMode.PercentOutput, amount)
+        backRight.set(ControlMode.PercentOutput, amount)
+    }
+
+    fun leftDistanceTraveled(): Double {
+        return frontLeft.selectedSensorPosition * Motors.FRONT_LEFT.encoderDistancePerPulse
+    }
+
+    fun rightDistanceTraveled(): Double {
+        return frontRight.selectedSensorPosition * Motors.FRONT_RIGHT.encoderDistancePerPulse
+    }
+
+    fun distanceTraveledAverage(): Double {
+        return (leftDistanceTraveled() + rightDistanceTraveled()) / 2.0
+    }
+
+    fun leftVelocity(): Double {
+        return frontLeft.selectedSensorVelocity * Motors.FRONT_LEFT.encoderDistancePerPulse
+    }
+
+    fun rightVelocity(): Double {
+        return frontLeft.selectedSensorVelocity * Motors.FRONT_RIGHT.encoderDistancePerPulse
+    }
+
+    fun velocityAverage(): Double {
+        return (rightVelocity() + leftVelocity()) / 2.0
+    }
+
+    fun tankDrive(leftSpeed: Double, rightSpeed: Double) {
+        spinLeft(leftSpeed)
+        spinRight(rightSpeed)
+    }
+
+    fun arcadeDrive(speed: Double, turn: Double) {
+        spinLeft(speed + turn)
+        spinRight(speed - turn)
+    }
+}
