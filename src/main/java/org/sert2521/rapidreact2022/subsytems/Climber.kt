85c22cf215b9e3add98cd37727a74fc2fce95742
kturner44 <kturner44@4j.lane.edu>
2022-01-23 17:03:13 -0800

    added encoders

--- src/main/java/org/sert2521/rapidreact2022/subsytems/Climber.kt
+++ src/main/java/org/sert2521/rapidreact2022/subsytems/Climber.kt
@@ -2,16 +2,18 @@ package org.sert2521.rapidreact2022.subsytems
 
 import edu.wpi.first.wpilibj.AnalogPotentiometer
 import edu.wpi.first.wpilibj.DigitalInput
+import edu.wpi.first.wpilibj.Encoder
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax
 import edu.wpi.first.wpilibj2.command.SubsystemBase
+import org.sert2521.rapidreact2022.Encoders
 import org.sert2521.rapidreact2022.LimitSwitches
 import org.sert2521.rapidreact2022.Motors
 import org.sert2521.rapidreact2022.Potentiometers
 
 //Add encoders
 object Climber : SubsystemBase() {
-    private val staticClimbMotor = PWMSparkMax(Motors.STATIC_CLIMBER.id)
-    private val variableClimbMotor = PWMSparkMax(Motors.VARIABLE_CLIMBER.id)
+    private val staticClimberMotor = PWMSparkMax(Motors.STATIC_CLIMBER.id)
+    private val variableClimberMotor = PWMSparkMax(Motors.VARIABLE_CLIMBER.id)
     private val variableActuator = PWMSparkMax(Motors.VARIABLE_ACTUATOR.id)
 
     private val staticDownLimitSwitch = DigitalInput(LimitSwitches.STATIC_CLIMBER_DOWN.id)
@@ -19,30 +21,45 @@ object Climber : SubsystemBase() {
     private val variableDownLimitSwitch = DigitalInput(LimitSwitches.VARIABLE_CLIMBER_DOWN.id)
     private val variableUpLimitSwitch = DigitalInput(LimitSwitches.VARIABLE_CLIMBER_UP.id)
 
+    private val staticClimberEncoder = Encoder(Encoders.STATIC_CLIMBER.idA, Encoders.STATIC_CLIMBER.idA, Encoders.STATIC_CLIMBER.reversed, Encoders.STATIC_CLIMBER.encodingType)
+    private val variableClimberEncoder = Encoder(Encoders.VARIABLE_CLIMBER.idA, Encoders.VARIABLE_CLIMBER.idA, Encoders.VARIABLE_CLIMBER.reversed, Encoders.VARIABLE_CLIMBER.encodingType)
+
     private val potentiometer = AnalogPotentiometer(Potentiometers.VARIABLE_CLIMBER_ANGLE.id, Potentiometers.VARIABLE_CLIMBER_ANGLE.maxAngle, Potentiometers.VARIABLE_CLIMBER_ANGLE.zeroAngle)
 
     init {
-        staticClimbMotor.inverted = Motors.STATIC_CLIMBER.reversed
-        variableClimbMotor.inverted = Motors.VARIABLE_CLIMBER.reversed
+        staticClimberMotor.inverted = Motors.STATIC_CLIMBER.reversed
+        variableClimberMotor.inverted = Motors.VARIABLE_CLIMBER.reversed
         variableActuator.inverted = Motors.VARIABLE_ACTUATOR.reversed
+
+        staticClimberEncoder.distancePerPulse = Encoders.STATIC_CLIMBER.encoderDistancePerPulse
+        variableClimberEncoder.distancePerPulse = Encoders.VARIABLE_CLIMBER.encoderDistancePerPulse
+
+        staticClimberEncoder.setMaxPeriod(Encoders.STATIC_CLIMBER.maxPeriod)
+        variableClimberEncoder.setMaxPeriod(Encoders.VARIABLE_CLIMBER.maxPeriod)
+
+        staticClimberEncoder.setMinRate(Encoders.STATIC_CLIMBER.minRate)
+        variableClimberEncoder.setMinRate(Encoders.VARIABLE_CLIMBER.minRate)
+
+        staticClimberEncoder.samplesToAverage = Encoders.STATIC_CLIMBER.samples
+        variableClimberEncoder.samplesToAverage = Encoders.VARIABLE_CLIMBER.samples
     }
 
     fun elevateStatic(speed: Double): Boolean {
         return if(staticDownLimitSwitch.get() or staticUpLimitSwitch.get()) {
-            staticClimbMotor.set(0.0)
+            staticClimberMotor.set(0.0)
             true
         }else{
-            staticClimbMotor.set(speed)
+            staticClimberMotor.set(speed)
             false
         }
     }
 
     fun elevateVariable(speed: Double): Boolean {
         return if(variableDownLimitSwitch.get() or variableUpLimitSwitch.get()) {
-            variableClimbMotor.set(0.0)
+            variableClimberMotor.set(0.0)
             true
         }else{
-            variableClimbMotor.set(speed)
+            variableClimberMotor.set(speed)
             false
         }
     }
