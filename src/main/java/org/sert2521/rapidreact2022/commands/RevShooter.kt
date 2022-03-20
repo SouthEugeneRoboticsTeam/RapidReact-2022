31f4ddcd08058c240c07cea11e18000a9e6e4fc7
kturner44 <kturner44@4j.lane.edu>
2022-03-20 14:10:38 -0700

    fix rev

--- src/main/java/org/sert2521/rapidreact2022/commands/RevShooter.kt
+++ src/main/java/org/sert2521/rapidreact2022/commands/RevShooter.kt
@@ -1,7 +1,6 @@
 package org.sert2521.rapidreact2022.commands
 
 import edu.wpi.first.wpilibj2.command.CommandBase
-import org.sert2521.rapidreact2022.RobotPreferences
 import org.sert2521.rapidreact2022.robotPreferences
 import org.sert2521.rapidreact2022.subsytems.Shooter
 
@@ -12,6 +11,7 @@ class RevShooter : CommandBase() {
 
     override fun initialize() {
         Shooter.setWheelSpeed(robotPreferences.shooterRPM)
+        Shooter.setWheelSpeedBack(robotPreferences.shooterBackRPM)
     }
 
     override fun end(interrupted: Boolean) {
